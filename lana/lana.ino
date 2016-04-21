e2c6b88227930a47eb1577e990b7e1e360ef1c64
Chanala Wilangka <c.wilangka@gmail.com>
2016-04-21 17:50:00 +0700

    experiment2

--- lana/lana.ino
+++ lana/lana.ino
@@ -5,14 +5,14 @@
     #include "Wire.h"
     #include <SoftwareSerial.h>
 
-    SoftwareSerial bluetooth(7, 8); // RX, TX
+    //SoftwareSerial bluetooth(7, 8); // RX, TX
     File myFile;
     const int chipSelect = 4;
     String sdfilename0;
     String sdfilename; 
     int LED= 12;  
     char input;
-    int inputbt;  
+    int flaginput = 0;
         //  Variables
     int sumpulse = 0;
     int BPMcount = 0;
@@ -74,7 +74,7 @@
       minute = 20;
       hour = 12;
       dayOfWeek = 4;
-      dayOfMonth =6;
+      dayOfMonth =12;
       month =3;
       year= 16;
       Wire.beginTransmission(DS1307_I2C_ADDRESS);
@@ -105,7 +105,7 @@
       month = bcdToDec(Wire.read());
       year = bcdToDec(Wire.read());
       txt_date=String(dayOfMonth,DEC)+"-"+String(month, DEC)+"-"+String(year, DEC);
-      txt_dateformat=String(dayOfMonth,DEC)+"/"+String(month, DEC)+"/"+String(year, DEC);
+      //txt_dateformat=String(dayOfMonth,DEC)+"\/"+String(month, DEC)+"\/"+String(year, DEC);
       //txt_time=String(hour,DEC)+":"+String(minute, DEC)+":"+String(second, DEC);
       txt_time=String(hour,DEC)+":"+String(minute, DEC);
       txt_min=String(minute, DEC);  
@@ -129,7 +129,6 @@
       pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
       pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
       Serial.begin(9600);             // we agree to talk fast!
-      bluetooth.begin(9600);
       pinMode(LED, OUTPUT);  
       Serial.println(">> START<<");  
       setDateDs1307(); //Set current time;
@@ -140,7 +139,7 @@
        // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
     //   analogReference(EXTERNAL);   
       
-     bluetooth.begin(9600);
+    // bluetooth.begin(9600);
 
     }  
       
@@ -156,7 +155,7 @@
        pul();
        bt(avgBPM);
        getDateDs1307();
-       sdfilename0 = txt_dateformat;
+       sdfilename0 = txt_date;
        sdfilename = sdfilename0+".txt" ;
        
     }  
@@ -191,6 +190,8 @@
       Serial.println("==========off==========");
      // delay(3600000);
       // 1min=60000 / 1 hour = 3600000
+      sd(sdfilename,avgBPM);
+      flaginput = 0; 
       flagpul = 1;
       sumpulse = 0;
       BPMcount = 0;
@@ -206,8 +207,10 @@
 
 /* Bluetooth *****************/ 
    void bt(int avgBPM){
-     bluetooth.println(String(avgBPM));
+   // input = bluetooth.read(); 
+    // bluetooth.println(String(avgBPM));
     // Keep reading from HC-05 and send to Arduino Serial Monitor
+    /*
   if (bluetooth.available()){
     Serial.write(bluetooth.read());
   }
@@ -215,9 +218,9 @@
   // Keep reading from Arduino Serial Monitor and send to HC-05
   if (Serial.available()){
     bluetooth.write(Serial.read());
-   // inputbt = Serial.read(); 
     
   }
+  */
 
 
     /*
@@ -252,7 +255,6 @@
      
 /* SD Card Function *****************/ 
 void sd(String sdfilename,int BPM){
-  
      Serial.print("Initializing SD card...\n");
      pinMode(SS, OUTPUT);
    
@@ -263,30 +265,38 @@ void sd(String sdfilename,int BPM){
       Serial.println("initialization done.");
   
        myFile = SD.open(sdfilename, FILE_WRITE);
+         Serial.println(String(sdfilename));
+       //saveToSdCard(sdfilename);
   
   // if open file has success, write data
-  if (myFile) {
-    Serial.print("Writing to "+String(sdfilename));
+  if(flaginput == 0){
+    if (myFile) {
+   Serial.print("Writing to "+String(sdfilename));
     myFile.println(sdfilename+" - "+txt_time+" - "+String(BPM)); //write data
     myFile.close(); // close file
     Serial.println(" done.");
-  } else {
-    Serial.println("error opening"+String(sdfilename));
+    } else {
+      Serial.println("error opening "+String(sdfilename));
+      }
+
+  } else if(flaginput == 1){
+        // Open file to read
+        myFile = SD.open(sdfilename); // Open file
+        if (myFile) {
+          Serial.println(String(sdfilename)+":");
+          // Read all data
+          while (myFile.available()) {
+          Serial.write(myFile.read());
+          }
+         myFile.close();
+        } else {
+          Serial.println("error opening "+String(sdfilename));
+        }
+         
   }
-  
-  // Open file to read
-  myFile = SD.open(sdfilename); // Open file
-  if (myFile) {
-    Serial.println(String(sdfilename)+":");
-    // Read all data
-    while (myFile.available()) {
-    Serial.write(myFile.read());
-    }
-   myFile.close();
-  } else {
-    Serial.println("error opening "+String(sdfilename));
-  }
-    Serial.println("=========================");
+ 
+   Serial.println("=========================");  
+
 }
 
 /* Custom filename to sdcard ***************/
